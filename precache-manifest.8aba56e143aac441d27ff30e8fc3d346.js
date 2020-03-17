4730a0fd3631e2e33f6b2c566c282ca1df11ee23
ElDugano <dmh838@hotmail.com>
2020-03-17 11:30:35 -0700

    Updates

---
+++ precache-manifest.8aba56e143aac441d27ff30e8fc3d346.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c711d8f4e1d587172eb180d1f82f822f",
+    "url": "/testreactwebsite/index.html"
+  },
+  {
+    "revision": "8dd190043d1bdf2a0e64",
+    "url": "/testreactwebsite/static/css/2.6bd962d6.chunk.css"
+  },
+  {
+    "revision": "e07a12b024e09a930b82",
+    "url": "/testreactwebsite/static/css/main.f5ca0da4.chunk.css"
+  },
+  {
+    "revision": "8dd190043d1bdf2a0e64",
+    "url": "/testreactwebsite/static/js/2.1820c4a5.chunk.js"
+  },
+  {
+    "revision": "95fe7b47190d8ffb46ace11b00922862",
+    "url": "/testreactwebsite/static/js/2.1820c4a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e07a12b024e09a930b82",
+    "url": "/testreactwebsite/static/js/main.b6937da3.chunk.js"
+  },
+  {
+    "revision": "754b09677e41cd586c43",
+    "url": "/testreactwebsite/static/js/runtime-main.a1536ff2.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/testreactwebsite/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/testreactwebsite/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/testreactwebsite/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/testreactwebsite/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/testreactwebsite/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testreactwebsite/static/media/logo.5d5d9eef.svg"
+  }
+]);
